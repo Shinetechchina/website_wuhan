e97d818fa9968beccd28381f26a15e091f768ea8
David Chen <darkbaby123@gmail.com>
2013-06-26 11:01:25 +0800

    Add js route and Load the box content via ajax
    * Add path.js, used by js route
    * Load the box contents via ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
+  layout -> (controller) { controller.action_name == 'index' ? 'application' : false }
+
   def index
+  end
+
+  def home
   end
 
   def if_forefront
   end
-
 end
