2768e4081788b2621ac8f3b2c672da720c025e66
LiuSihao <liusihao123@qq.com>
2013-07-03 15:41:32 +0800

    add image version to images tab

--- app/decorators/models/refinery/image_decorator.rb
+++ app/decorators/models/refinery/image_decorator.rb
@@ -1,9 +1,16 @@
 Refinery::Image.class_eval do
+  SMALL_SIZE_MAX = 22500
+  MEDIUM_SIZE_MAX = 140000
+
+  scope :small, where("image_width * image_height <= #{SMALL_SIZE_MAX}")
+  scope :medium, where("image_width * image_height <= #{MEDIUM_SIZE_MAX}")
+  scope :large, where("image_width * image_height > #{MEDIUM_SIZE_MAX}")
+
   def version
     area = self.image_width * self.image_height
-    if area <= 22500
+    if area <= SMALL_SIZE_MAX
       return 'small'
-    elsif area <= 140000
+    elsif area <= MEDIUM_SIZE_MAX
       return 'medium'
     else
       return 'large'
