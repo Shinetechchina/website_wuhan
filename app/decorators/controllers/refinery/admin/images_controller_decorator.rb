183859c315562077db3c30f5ab0ceed629068474
David Chen <darkbaby123@gmail.com>
2013-07-08 13:19:24 +0800

    Fix - heroku precompile failure

--- app/decorators/controllers/refinery/admin/images_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-Refinery::Admin::ImagesController.class_eval do
-  #TODO scope image version dependent current page
-  #def paginate_images
-  #  @images = @images.paginate(:page => params[:page],
-  #                             :per_page => Refinery::Image.per_page(from_dialog?,!@app_dialog))
-  #end
-
-end
