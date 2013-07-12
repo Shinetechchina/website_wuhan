6e7d317e2473fe7236814c13a929f023d9786aa5
David Chen <darkbaby123@gmail.com>
2013-07-12 15:51:38 +0800

    Add tag to js route

--- vendor/extensions/staffs/app/controllers/refinery/staffs/staffs_controller.rb
+++ vendor/extensions/staffs/app/controllers/refinery/staffs/staffs_controller.rb
@@ -3,6 +3,7 @@ module Refinery
     class StaffsController < ::ApplicationController
       def index
         @staffs = Staff.order('position ASC')
+        @staffs = @staffs.tagged_with(params[:tag], any: true) if params[:tag].present?
         @page = ::Refinery::Page.where(:link_url => "/staff").first
         render layout: false
       end
