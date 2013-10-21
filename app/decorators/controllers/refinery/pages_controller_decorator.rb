b8296ba4f078b052757b4bd1075f72ec7f6d1b9c
LiuSihao <liusihao123@qq.com>
2013-10-21 15:29:46 +0800

    add use cookies notice in home page

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -26,6 +26,7 @@ Refinery::PagesController.class_eval do
     #   render layout: false
     # end
 
+    flash[:notice] = "Our site uses your cookies for analytics and improve user experiences."
     find_boxes('/')
   end
 
